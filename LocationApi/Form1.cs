e67eac005c1795405bd249ebb5c3925527f2b290
Hamza Ipek <hamzaipekk@outlook.com>
2022-12-15 21:55:09 +0300

    Add project files.

---
+++ LocationApi/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LocationApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            XmlTextReader okuyucum = new XmlTextReader("http://ip-api.com/xml");
+            while(okuyucum.Read())
+            {
+                if(okuyucum.NodeType == XmlNodeType.Element)
+                { 
+                    if(okuyucum.Name == "country")
+                    {
+                        okuyucum.Read();
+                        richTextBox1.Text += "Ülke:" + okuyucum.Value.ToString() + "\n";
+                    }
+                    if (okuyucum.Name == "regionName")
+                    {
+                        okuyucum.Read();
+                        richTextBox1.Text += "Sehir:" + okuyucum.Value.ToString() + "\n";
+
+                    }
+                    if (okuyucum.Name == "query")
+                    {
+                        okuyucum.Read();
+                        richTextBox1.Text += "IP:" + okuyucum.Value.ToString() + "\n";
+
+                    }
+                }
+            }
+            okuyucum.Close();
+        }
+    }
+}
